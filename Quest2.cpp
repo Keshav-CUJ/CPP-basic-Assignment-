60dbdfae7aa8f84d79fc296d4fa65f854b3211e5
Keshav bhatt <166917287+Keshav-CUJ@users.noreply.github.com>
2024-07-22 15:17:44 +0530

    Question 2 code and output

---
+++ Quest2.cpp
@@ -0,0 +1,7 @@
+//Write a program to print 10 using 2 positive numbers less than 6 in C++ as output.
+#include<iostream>
+int main(){
+    int a = 5,b = 2;
+    std::cout<<"Output is \n"<<a*b;
+    return 0;
+}
